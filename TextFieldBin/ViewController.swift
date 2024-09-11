3a7d043217c938760c402850f9e93d2bf0d8444e
GyuBin Hwang <spiraldailydev@gmail.com>
2024-09-11 17:29:44 +0900

    add textfield custom method

--- TextFieldBin/ViewController.swift
+++ TextFieldBin/ViewController.swift
@@ -16,5 +16,56 @@ class ViewController: UIViewController, UITextFieldDelegate {
         textField.delegate = self
      }
     
+    @IBAction func doneButtonTapped(_ sender: UIButton) {
+        textField.resignFirstResponder()
+        
+    }
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        print(#function)
+        return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print(#function)
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        print(#function)
+        return true
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        print(#function)
+        print("텍스트필드의 입력값: \(string)")
+        
+        if Int(string) != nil { return false
+        } else {
+            guard let text = textField.text else { return true }
+            let newLength = text.count + string.count - range.length
+            return newLength <= 10
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print(#function)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        print(#function)
+
+        if textField.text == "" {
+            textField.placeholder = "Type Something!"
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print(#function)
+        textField.text = ""
+    }
 }
 
